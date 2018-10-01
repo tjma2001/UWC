6db94c13b05f49bd7a81809147b5d81ac4076d72
Taariq Mullins <tjma2001@gmail.com>
2018-10-01 13:12:35 +0200

    updated part 3

--- Day 5/part 3/script.js
+++ Day 5/part 3/script.js
@@ -7,6 +7,7 @@ window.onload = function () {
         var name = getName()
         var surname = getSurname()
         var age = getAge()
+        var phone = getPhone()
 
         if(age < 18) {
             alert("You are not old enough to use this form")
@@ -14,8 +15,11 @@ window.onload = function () {
         else if(name == "" || name == undefined) {
             alert("you must enter a name")
         }
-        else if(surname = "" || surname == undefined) {
+        else if(surname == "" || surname == undefined) {
             alert("you must enter a surname")
+        }
+        else if(phone.length !== 10) {
+            alert("phone number must be 10 digits long")
         }
         else {
             alert(
@@ -51,4 +55,13 @@ function getSurname() {
 function getAge() {
     var age = document.getElementById('age')
     return age.value
+}
+
+/**
+ * This function gets the phone
+ * number form the phone input
+ */
+function getPhone() {
+    var phone = document.getElementById('phone')
+    return phone.value
 }
