563f824ee2e60e2d80dcad06fa687496b589b4ec
Taariq Mullins <tjma2001@gmail.com>
2018-11-08 12:56:33 +0200

    refactor: guess prac part 1

--- Mobile app development with the web/guessing/practical/script.js
+++ Mobile app development with the web/guessing/practical/script.js
@@ -1,7 +1,50 @@
+function generateGuess() {
+  return Math.round(Math.random() * 10)
+}
+
+const timerValue = 5
+
 const app = new Vue({
   el: "#app",
   data: {
-    timer: 0,
-    errors: [ true, true]
+    timer: timerValue,
+    computerGuess: generateGuess(),
+    userGuess: 0,
+    errors: [ ],
+    showModal: false,
+    failure: false,
+    success: false,
+    customMessage: ''
+  },
+  methods: {
+    enter: function () {
+      console.log(this.computerGuess)
+      if(Number(this.userGuess) === this.computerGuess) {
+        this.errors = []
+        this.timer = timerValue
+        this.computerGuess = generateGuess()
+      } else {
+        this.errors.push(true)
+        if(this.errors.length >= 3) {
+          alert('you failed')
+          this.errors = []
+          this.timer = timerValue
+          this.computerGuess = generateGuess()
+        }
+      }
+    }
   }
 })
+
+function countDown() {
+  app.timer--
+  if(app.timer === 0) {
+    app.showModal = true
+    app.failure = true
+    app.success = false
+    app.timer = timerValue
+    app.customMessage = "The correct value was " + app.computerGuess
+  }
+}
+
+setInterval(countDown, 1000)
