1224dbec74fe76bfa572de89928619817e1ec612
Taariq Mullins <tjma2001@gmail.com>
2018-11-08 10:27:07 +0200

    feat: added practical

--- Mobile app development with the web/vue1/practical/script.js
+++ Mobile app development with the web/vue1/practical/script.js
@@ -1,1 +1,14 @@
-console.log("running script")
+var app = new Vue({
+  el: '#app',
+  data: {
+    guess: 0,
+    userGuess: 0
+  },
+  methods: {
+    
+  }
+})
+
+app.guess = Math.round(Math.random() * 10)
+
+console.log(app.guess)
