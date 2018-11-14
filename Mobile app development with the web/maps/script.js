6b65a825f03c6774ff42f60ac2670e8506fc5d03
Taariq Mullins <tjma2001@gmail.com>
2018-11-14 12:54:35 +0200

    refactor: maps

--- Mobile app development with the web/maps/script.js
+++ Mobile app development with the web/maps/script.js
@@ -1,46 +1,47 @@
 const appId = 'BqU0U8dHPxCCYD9lLONy'
 const appCode = 'sEcZjhBTHp46SlRzts1ZMQ'
 
-const url = "http://autocomplete.geocoder.api.here.com/6.2/suggest.json" +
+const autocompleteUrl = "http://autocomplete.geocoder.api.here.com/6.2/suggest.json" +
   "?app_id=" + appId +
   "&app_code=" + appCode +
-  "&query=";
-
-function b() {
-  this.newstate = 'b'
-  console.log('b', this)
-}
-
-function a() {
-  this.state = 'a'
-  console.log('a', this)
-  b()
-}  
-
-a()
-
+  "&query="
+  
+const geocodeUrl = "https://geocoder.api.here.com/6.2/geocode.json" +
+  "?app_id=" + appId +
+  "&app_code=" + appCode +
+  "&searchtext="
 
 var app = new Vue({
   el: '#app',
   data: {
     address: '',
-    results: []
+    results: [],
+    geoResults: []
   },
   methods: {
+    find: function () {
+      var _this = this
+      fetch(geocodeUrl + this.address)
+        .then(function (response) {
+          return response.json()
+        })
+        .then(function (response) {
+          console.log('geocode', response)
+          console.log('location', response.Response.View[0].Result[0].Location.DisplayPosition)
+          _this.geoResults = response.Response.View[0].Result
+        })
+    },
     search: function () {
       if(this.address.length > 5) {
         var _this = this
-        console.log('search', this)
-        fetch(url + this.address)
+        fetch(autocompleteUrl + this.address)
           .then(function (response) {
             return response.json()
           })
           .then(function (response){
             _this.results = response.suggestions
           })
-        
       } else {
-        // do some error handling
         console.log('must use a valid address')
       }
     },
